84c06d731e4ce5c32d1abaf6112882dc35372ef0
Vaishnavi834 <vaishnavitatigunta@gmail.com>
2024-09-04 21:33:39 +0530

    Add files via upload

---
+++ 4/4_9_24.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main();
+float a,b;
+printf("enter two numbers:");
+scanf("%f%f",&a,&b);
+printf("the sum is:%f",a+b);
+printf("the difference is:%f",a-b);
+printf("the product is:%f",a*b);
+printf("division of a and b is:%f",a/b);
+return 0;
+}
+
