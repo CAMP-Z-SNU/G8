c00200479445b8efb7182f931eb9160a9af5c267
yashaswini2007 <devarintimadhavilatha@gmail.com>
2024-09-04 22:05:48 +0530

    Add files via upload

---
+++ 1/4_9_24.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float a=0,b=0;
+printf("enter 2 integers:");
+scanf("%f%f",&a,&b);
+printf("the sum is:%f",a+b);
+printf("the difference is:%f",a-b);
+printf("the product is:%f",a*b);
+printf("the ratio is:%f",a/b);
+}
